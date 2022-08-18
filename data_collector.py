1ebecafc93cfb410ef6db48b223bda710dd7e476
Prabhat-Kumar-Singh <pksiit2000@gmail.com>
2022-08-18 14:07:15 +0530

    Data Collector

--- data_collector.py
+++ data_collector.py
@@ -5,7 +5,7 @@
 import glassdoor_scraper as gs 
 import pandas as pd 
 
-path = "C:/Users/HP/Documents/ds_salary_project/chromedriver"
+path = "C:/Users/HP/Documents/DS_SALARY/chromedriver"
 
 df = gs.get_jobs('data scientist',1000, False, path, 15)
 
